b5461a1f95d46aca2cb3e4672c9dc6497801c9c5
jpschoen <jpschoen@gmail.com>
2017-02-22 11:17:21 -0500

    added network stat graphs

--- Code/fdi_descriptive.R
+++ Code/fdi_descriptive.R
@@ -58,88 +58,86 @@ fdi <- fdi[,c(1,3,2,4:36)]
 #log FDI
 fdi$ln_Value <- log(fdi$Value+1)
 
-# create rho function
-r <- function(g){
-  diag(g) <- 0
-  stat<- sum(g*t(g))/sum(g)
-  stat
-  # g is an adjacency matrix
-  ## g has no self-loops
-  # calculate r
-}
+
 # rho as reciprocity
 rho <- function(g){
   g2 <- g
   diag(g2) <- NA
-  (r(g)-mean(g2, na.rm=T))/(1-mean(g2, na.rm=T))
-  # g is an adjacency matrix
-  ## g has no self-loops
-  # calculate rho
+  g_mean <- mean(g2, na.rm=T)
+  num <- sum((g-g_mean)*t(g-g_mean))
+  denom <- sum((g-g_mean)^2)
+  stat <- num/denom
+  stat
 }
-# rho as reciprocity for weighted adjacency matrix
-rho <- function(e){
-  (((sum(e))/(length(e)*(length(e)-1)))^2)/((sum(e^2))/(length(e)*(length(e)-1)))
-}
-
 
 #descriptive stats by whole network#######################
 
 descriptive_stats <- matrix(nrow=12, ncol=6)
 
 for(i in 1:12){
-
-descriptive_stats[i,1] <- i +2000  
-
-#extract one year
-fdi_yr <- subset(fdi, fdi$Year ==i +2000)
-# create graph object
-fdi_graph <- graph.data.frame(fdi_yr, directed=TRUE, vertices=NULL)
-fdi_y <- get.adjacency(fdi_graph, attr='ln_Value', names=TRUE, sparse=FALSE)
-
-#measure reciprocity
-descriptive_stats[i,2] <- rho(fdi_yr$ln_Value)
-
-# Transitivity
-fdi_g <- graph.adjacency(fdi_y, mode="directed")
-t <- transitivity(fdi_g, type="weighted", vids=NULL,isolates="NaN")
-t <- na.omit(t)
-descriptive_stats[i,3] <- mean(t)
-descriptive_stats[i,4] <- max(t)
-descriptive_stats[i,5] <- min(t)
-
-# assortativity by Polity
-library(doBy)
-agg<-summaryBy(Dest.polity~ Destination, fdi_yr)
-# Attach attribute Polity to each vertex
-fdi_g<-set_vertex_attr(fdi_g,"Polity",value=agg$Dest.polity.mean)
-list.vertex.attributes(fdi_g)
-# Calculate the assortativity coefficient for polity
-descriptive_stats[i,6] <- assortativity(fdi_g,types1=V(fdi_g)$Polity,directed=T)
-
+  
+  descriptive_stats[i,1] <- i +2000  
+  
+  #extract one year
+  fdi_yr <- subset(fdi, fdi$Year ==i +2000)
+  # create graph object
+  fdi_graph <- graph.data.frame(fdi_yr, directed=TRUE, vertices=NULL)
+  fdi_y <- get.adjacency(fdi_graph, attr='ln_Value', names=TRUE, sparse=FALSE)
+  
+  #measure reciprocity
+  descriptive_stats[i,2] <- rho(fdi_y)
+  
+  # Transitivity
+  fdi_g <- graph.adjacency(fdi_y, mode="directed")
+  t <- transitivity(fdi_g, type="weighted", vids=NULL,isolates="NaN")
+  t <- na.omit(t)
+  descriptive_stats[i,3] <- mean(t)
+  descriptive_stats[i,4] <- max(t)
+  descriptive_stats[i,5] <- min(t)
+  
+  # assortativity by Polity
+  library(doBy)
+  agg<-summaryBy(Dest.polity~ Destination, fdi_yr)
+  # Attach attribute Polity to each vertex
+  fdi_g<-set_vertex_attr(fdi_g,"Polity",value=agg$Dest.polity.mean)
+  list.vertex.attributes(fdi_g)
+  # Calculate the assortativity coefficient for polity
+  descriptive_stats[i,6] <- assortativity(fdi_g,types1=V(fdi_g)$Polity,directed=T)
+  
 }
 
 
-# plot stats
+# plot network stats
 library(plotly)
+m <- list(
+  l = 50,
+  r = 50,
+  b = 50,
+  t = 50,
+  pad = 1
+)
 
 data <- data.frame(descriptive_stats)
 
 plot_ly(data, x = ~X1, y = ~X2, type = 'scatter', mode = 'lines') %>%
-layout(title = "FDI Network Reciprocity",
-       xaxis = list(title = "Year"),
-       yaxis = list (title = "Reciprocity"))
+  layout(title = "FDI Network Reciprocity",
+         xaxis = list(title = "Year"),
+         yaxis = list (title = "Reciprocity"),
+         margin = m)
 
 plot_ly(data, x = ~X1, y = ~X3, type = 'scatter', mode = 'lines') %>%
   layout(title = "FDI Network Transitivity",
          xaxis = list(title = "Year"),
-         yaxis = list (title = "Transitivity"))
+         yaxis = list (title = "Transitivity"),
+         margin = m)
 
 plot_ly(data, x = ~X1, y = ~X6, type = 'scatter', mode = 'lines') %>%
   layout(title = "Assortativity for Polity",
          xaxis = list(title = "Year"),
-         yaxis = list (title = "Assortativity"))
+         yaxis = list (title = "Assortativity"),
+         margin = m)
 
-
+# plot network
 
 
 #descriptive of covariates#######################
