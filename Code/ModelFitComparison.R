9cb55c4a54ce2f23be63167d6eac4551e707cfd9
bbd5087 <bbd5087@PLSCHOMMB011.local>
2017-03-30 16:19:25 -0400

    updated BIC plots to use transitive weights models

--- Code/ModelFitComparison.R
+++ Code/ModelFitComparison.R
@@ -3,13 +3,18 @@
 library(ergm)
 
 # Get file names for ERGM results
-modelFiles <- dir("./Code/fdi_models")
+modelFiles <- dir("./Code/models_tweight")
+modelFiles <- modelFiles[which(grepl("rda",modelFiles))]
 
 # Matrix to store results
 BICMat <- matrix(0,length(modelFiles)/2,2)
 
 # Row labels for fit matrix
-rowLabels <- sort(unique(substr(modelFiles,7,8)))
+
+cols <- 1+grepl("model2",modelFiles)
+
+
+rowLabels <- sort(unique(substr(modelFiles,6+cols,7+cols)))
 rownames(BICMat) <- rowLabels
 colnames(BICMat) <- c("Independent","Network")
 
@@ -20,13 +25,13 @@ for(i in 1:length(modelFiles)){
 	fiti <- new.env()
 	
 	# load the fit in the ith model file
-	load(paste("./Code/fdi_models/",modelFiles[i],sep=""),envir=fiti)
+	load(paste("./Code/models_tweight/",modelFiles[i],sep=""),envir=fiti)
+    
+    # Identify whether there are network effects based on file name
+    col <- 1+grepl("model2",modelFiles[i])
 	
 	# Extract the year from the file name
-	yri <- substr(modelFiles,7,8)[i]
-	
-	# Identify whether there are network effects based on file name
-	col <- 2-1*(nchar(modelFiles[i])==15)
+	yri <- substr(modelFiles,6+col,7+col)[i]
 	
 	# Store results
 	BICMat[match(yri,rowLabels),col] <- BIC(fiti[[ls(fiti)]])
