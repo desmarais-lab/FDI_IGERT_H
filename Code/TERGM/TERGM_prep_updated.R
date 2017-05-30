827a485a2a0cde34e96ae4d7b7414fd720fbe26c
jpschoen <jpschoen@gmail.com>
2017-05-30 12:56:49 -0500

    added meta ERGM

---
+++ Code/TERGM/TERGM_prep_updated.R
@@ -0,0 +1,90 @@
+# clear workspace
+rm(list=ls())
+
+set.seed(19)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+#load packages
+library(magic)
+library(network)
+library(igraph)
+library(doBy)
+library(plyr)
+
+#setwd("/Users/johnpschoeneman/Desktop/ACI/Count")
+
+#load in data
+fdi <- read.csv("sub_stock.csv", stringsAsFactors=FALSE)        #FDI
+fdi <- fdi[,-1]
+#125 countries, 12 years (2001-2012),
+fdi <- fdi[,c(2,1,3:44)]
+
+range_0to1 <- function(x){(x-min(x))/(max(x)-min(x))}
+
+#scale continuous variables
+vars <- c(18:34,36, 38:39, 41:44)
+for(i in vars){
+  
+  fdi[,i] <- range_0to1(fdi[,i]) 
+  
+}
+
+#subset by year, get adjacency matrix, and create list of DV matrices #########################################
+
+#FDI Stock
+
+# Create an empty list in which to store the networks
+netlist <- list()
+years <- 2002:2012
+
+for(i in 1:11){
+#subset by year
+fdi_yr <- subset(fdi, fdi$Year == years[i])
+#turn into graph object
+fdi_graph <- graph.data.frame(fdi_yr)
+#extract adjacency matrix
+full <- as.network(get.adjacency(fdi_graph,attr='Value_ln', sparse=FALSE))
+#loop and add to list
+netlist[[i]] <- full
+
+# add all the vertex attributes to the networks
+vertex <- summaryBy(Origin.GDP+Origin.polity+Origin.TO+Origin.pop+Origin.GDPg+ 
+                      Origin.GDPpc+Origin.pv ~ Origin, data=fdi_yr)
+names(vertex) <- c("name","GDP", "Polity", "TradeOpen", "Pop", "GDP.g","GDPpc", "PV")
+
+
+netlist[[i]] %v% "polity" <- vertex$Polity
+netlist[[i]] %v% "trade_opennes" <- vertex$TradeOpen
+
+
+}
+
+
+
+#lag FDI Stock, mass, and distance
+
+covlist <- list()
+
+for(i in 1:11){
+  #subset by year
+  fdi_yr <- subset(fdi, fdi$Year == years[i])
+  #turn into graph object
+  fdi_graph <- graph.data.frame(fdi_yr)
+  #extract adjacency matrix
+  lag <- get.adjacency(fdi_graph,attr='Value_ln.1', sparse=FALSE)
+  mass <- get.adjacency(fdi_graph,attr='mass', sparse=FALSE)
+  dist <- get.adjacency(fdi_graph,attr='dist', sparse=FALSE)
+  #put covariates into list
+  covlist_yr <- list(lag=lag, mass=mass, dist=dist)
+  #add to main list
+  covlist[[i]] <- covlist_yr
+}
+
+
+#Clean out unneeded data
+rm(dist, fdi, fdi_yr, full, lag, mass, vertex, covlist_yr, fdi_graph, i, vars, years)
+#SAVE as Rdata
+save(covlist, file = "fdi_cov.Rdata")
+save(netlist, file = "fdi_net.Rdata")
+
---
+++ Code/TERGM/TERGM_updated.R
@@ -0,0 +1,126 @@
+# clear workspace
+rm(list=ls())
+
+set.seed(19)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+ergmPooledMeta <- function(list_of_networks,ergm_call,ergm_call_no_offset,list_of_edgecovs=NULL,seed=1234,ncores=2){
+  require(ergm)
+  require(ergm.count)
+  require(doParallel)
+  require(foreach)
+  
+  # ergm_call should be an expression
+  
+  # time fixed effects are required
+  
+  # formula should include all pooled terms in offset()
+  
+  # network should be called 'net' in the formula
+  
+  # edgecovs should be a T x Number_of_edgecovs length named list of matrices
+  # , where names(list_of_edgecovs[[t]])[i] is the matrix name used in the
+  # model formula
+  
+  ## Doing individual ergm estimation for pooling
+  registerDoParallel(cores=ncores)
+  ergms <- foreach(i=1:length(list_of_networks)) %dopar% {
+    net <- list_of_networks[[i]]
+    if(length(list_of_networks)==length(list_of_edgecovs)){
+      edgecovars <- list_of_edgecovs[[i]]
+    }
+    set.seed(seed)
+    eval(ergm_call_no_offset)
+  }
+  stopImplicitCluster()
+  
+  print("finished stage 1")
+  
+  coefMat <- NULL
+  weightMat <- NULL
+  meta.coef <- 0
+  varDenom <- 0
+  for(i in 1:length(list_of_networks)){
+    if(length(list_of_networks)==length(list_of_edgecovs)){
+      edgecovars <- list_of_edgecovs[[i]]
+    }
+    coefMat <- rbind(coefMat,coef(ergms[[i]]))
+    weightMat <- rbind(weightMat,1/(summary(ergms[[i]])$asyse)^2)
+    varDenom <- varDenom + weightMat[i,]
+    meta.coef <- meta.coef + coefMat[i,]*weightMat[i,]
+  }
+  meta.coef <- meta.coef/varDenom
+  
+  SE <- sqrt(1/varDenom)
+  
+  pooled_par <- meta.coef[-1]
+  
+  registerDoParallel(cores=ncores)
+  ergms.pooled <- foreach(i=1:length(list_of_networks)) %dopar% {
+    net <- list_of_networks[[i]]
+    if(length(list_of_networks)==length(list_of_edgecovs)){
+      edgecovars <- list_of_edgecovs[[i]]
+    }
+    set.seed(seed)
+    eval(ergm_call)
+  }
+  stopImplicitCluster()
+  
+  print("finished stage 2")
+  
+  coefs <- meta.coef[-1]
+  SE  <- SE[-1]
+  loglik <- 0
+  for(i in 1:length(ergms.pooled)){
+    if(length(list_of_networks)==length(list_of_edgecovs)){
+      edgecovars <- list_of_edgecovs[[i]]
+    }
+    loglik <- loglik + as.numeric(logLik(ergms.pooled[[i]]))
+    coefs <- c(coefs,coef(ergms.pooled[[i]])[1])
+    SE <- c(SE,summary(ergms.pooled[[i]])$asyse[1])
+  }
+  
+  AIC <- 2*length(coefs)-2*loglik
+  
+  list(estimate=coefs,SE=SE,AIC=AIC,pooled.ergms=ergms.pooled)
+  
+}
+
+
+### Load Libraries
+library(ergm)
+
+### Read in data
+load("fdi_cov.Rdata")
+load("fdi_net.Rdata")
+
+
+# the main ergm call is a complete call to ERGM wrapped in the expression() function. The differences for use in
+# the ergmPooledMeta function are two-fold. First, all terms beside the edges term are wrapped in the 'offset()'
+# function. Second, offset.coef is defined as pooled_par. These two differences are necessary for estimating the
+# partially pooled models.
+# note that edge covariates should be specified as edgecov(edgecovars[["nameOfMatrix"]])
+ergm.call <- expression(ergm(net ~ edges + offset(esp(0))+offset(nodecov("polity"))+
+                               offset(nodecov("trade_opennes"))+offset(edgecov(edgecovars[[1]]))+
+                               offset(edgecov(edgecovars[[2]]))+offset(edgecov(edgecovars[[3]]))
+                             ,offset.coef=pooled_par))
+
+# the no offset call strips the ERGM call of the offset() function and the offset.coef argument
+# terms must match and be in the same order
+ergm.call.no.offset <- expression(ergm(net ~ edges + esp(0)+
+                                         nodecov("polity")+nodecov("trade_opennes")+
+                                         edgecov(edgecovars[[1]])+
+                                         edgecov(edgecovars[[2]])+
+                                         edgecov(edgecovars[[3]])
+                                       ))
+
+# the partially pooled ergm conducts a two-step meta analysis
+# in the first step, coefficients are estimated for each network individually
+# in the second step, all effects are fixed at the fixed effects meta analysis estimate, and only the
+# intercepts are estimated for each network
+# The results include the pooled coefficients (note network-specific intercepts are at the end), SEs, AIC,
+# and the ergm opjects from the second stage of pooled estimation
+# MCMC diagnostics can be run on the second-stage ERGM results.
+pooledERGMres <- ergmPooledMeta(list_of_networks=netlist,ergm_call=ergm.call,ergm_call_no_offset=ergm.call.no.offset,list_of_edgecovs=covlist)
+
