3d9815ef6044a87f1214d47fd553d145d41934d8
jpschoen <jpschoen@gmail.com>
2019-12-09 20:26:45 -0600

    updated SI code

--- Code/supplementary/AMEN/LSM_rl_plots.R
+++ Code/supplementary/AMEN/LSM_rl_plots.R
@@ -1,51 +1,60 @@
 # clear workspace, setting random seed, and setting work directory
-#rm(list=ls())
-#set.seed(19)
-#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+rm(list=ls())
+set.seed(19)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 library(ggplot2)
-library(ggthemes)
-library(broom)
+
 
 
 #load ERGM results for with
 load("LSM_nrm_results.RData")
 
 #check convergence
-for(i in 1:11){
-  beta.mcmc <- as.mcmc(LSMs[[i]]$BETA)
-  print(geweke.diag(beta.mcmc))
+#for(i in 1:11){
+#  beta.mcmc <- as.mcmc(LSMs[[i]]$BETA)
+#  print(geweke.diag(beta.mcmc))
+#}
+
+confint_ame <- function(x, conf){
+  df <- as.data.frame(matrix(nrow= ncol(x),ncol=5))
+  colnames(df) <- c("var", "estimate", "error","conf.low", "conf.high")
+  df[,1] <- colnames(x)
+  n <- nrow(x)-1
+  for(r in 1:nrow(df)){df[r,2] <- mean(x[,r])}
+  for(r in 1:nrow(df)){df[r,3] <- qt(conf,df=n-1)*sd(x[,r])/sqrt(n)}
+  for(r in 1:nrow(df)){df[r,4] <- df[r,2]-df[r,3]}
+  for(r in 1:nrow(df)){df[r,5] <- df[r,2]+df[r,3]}
+  return(df)
 }
+
 
 #put results into df
 years <- 2002:2012
-df <- tidy(ergms[[1]], conf.int=TRUE)
+df <- confint_ame(LSMs[[1]]$GOF, conf=.975)
 df$year <- 2002
 for(i in 2:11){
-  dfi <- tidy(ergms[[i]], conf.int=TRUE)
+  dfi <- confint_ame(LSMs[[i]]$GOF, conf=.975)
+  dfi$year <- years[i]
+  df <- rbind(df, dfi)
+}
+for(i in 1:11){
+  dfi <- confint_ame(LSMs[[i]]$BETA, conf=.975)
   dfi$year <- years[i]
   df <- rbind(df, dfi)
 }
 
 
 
-
 #set vars to loop through.
-vars <- c("Dest_Polity", "Origin_Polity", "Dest_TO","Origin_TO","Dest_GDPpc", "Origin_GDPpc",
-          "LDV", "Mass", "Distance", "Non_Defense_Treaty", "Defense_Treaty", 
-          "Trade_Volume", "Bilateral_Investment_Treaty","both_OECD", "PTA_Depth")
+vars <- unique(df$var)
 
-
-for(i in list_vars){
-  df <- subset(df_50, df_50$term==df_names[i])
-  df$year <- ifelse(df$model=="with", df$year+.1, df$year)
-  df$year <- ifelse(df$model=="without", df$year-.1, df$year)
-  name <- ggplot(df, aes(x=year, y = estimate, ymin= conf.low, ymax= conf.high,
-                         colour=model,legend=TRUE, shape=model), legend=TRUE)+
+for(i in 1:21){
+  df2 <- subset(df, df$var==vars[i])
+  name <- ggplot(df2, aes(x=year, y = estimate, ymin= conf.low, ymax= conf.high,
+                         colour="#D55E00",legend=TRUE), legend=TRUE)+
     geom_pointrange(size= .5, fatten = 1.25)+ geom_hline(yintercept = 0)+
     scale_x_continuous(breaks= seq(2002,2012,3))+
-    scale_color_manual(breaks = c("W/", "W/"),
-                       values=c("#D55E00", "#000000"))+  
     xlab("Year") + ylab("Coefficient")+ 
     theme(axis.text  = element_text(family="Times", size=9),
           axis.title  = element_text(family="Times", size=8),
@@ -54,6 +63,6 @@ for(i in list_vars){
           plot.title = element_text(family="Times"),
           legend.position="none")
   #create plot object
-  ggsave(paste("figures/main_rl_plots/",vars[i],".pdf", sep=""), name, device="pdf", width=2.85, height=2)
+  ggsave(paste("LSM_rl_plots/",vars[i],".pdf", sep=""), 
+         name, device="pdf", width=2.565, height=1.8)
 }
-
--- Code/supplementary/Descriptive/SI_sectionA.R
+++ Code/supplementary/Descriptive/SI_sectionA.R
@@ -4,67 +4,33 @@ set.seed(19)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 #load packages
-library(magic)
-library(network)
-library(igraph)
 library(doBy)
-library(plyr)
-library(DataCombine)
-
-
-
+library(ggplot2)
+library(xtable)
 #load in data
-fdi <- read.csv("sub_stock.csv", stringsAsFactors=FALSE)        #FDI
+fdi <- read.csv("../../main/subset_stock2.csv", stringsAsFactors=FALSE) #FDI
 fdi <- fdi[,-1]
 
-# create new variable transformations
-fdi$trade_ln <- log(fdi$trade_int+fdi$trade_hco+fdi$trade_cap+fdi$trade_mix+1) 
-fdi$dyad <- paste(fdi$Destination, fdi$Origin, sep = "")
-fdi$mass <- (log(fdi$Dest.GDP*fdi$Origin.GDP))
-fdi$dist_ln <- log(fdi$dist)
-fdi$Value_ln <- round(log(ifelse(fdi$Value<0, 0, fdi$Value)+1)) 
-fdi$Dest.pop_ln <- log(fdi$Dest.pop)
-fdi$Origin.pop_ln <- log(fdi$Origin.pop)
-
-#lag DV and GDP (to create growth rates)
-fdi <- slide(fdi, Var = "Dest.GDP", GroupVar = "dyad", slideBy = -1)
-fdi <- slide(fdi, Var = "Origin.GDP", GroupVar = "dyad", slideBy = -1)
-fdi <- slide(fdi, Var = "Value_ln", GroupVar = "dyad", slideBy = -1)
-
-#Create GDP pc (logged) and growth rate
-fdi$Origin.GDPpc_ln <- log(fdi$Origin.GDP/fdi$Origin.pop)
-fdi$Dest.GDPpc_ln <- log(fdi$Dest.GDP/fdi$Dest.pop)
-fdi$Dest.GDPpc <- fdi$Dest.GDP/fdi$Dest.pop
-fdi$Origin.GDPg <- (fdi$Origin.GDP-fdi$`Origin.GDP-1`)/fdi$`Origin.GDP-1`
-fdi$Dest.GDPg <- (fdi$Dest.GDP-fdi$`Dest.GDP-1`)/fdi$`Dest.GDP-1`
-
-
-# create alliance dummy
-fdi$alliance <- (fdi$nonaggression.max.x + fdi$entente.max.x + fdi$neutrality.max.x)
-fdi$alliance <- as.numeric(ifelse(fdi$alliance >0, 1, 0))
-#make bit  and defense numeric
-fdi$bit_dummy <- as.numeric(fdi$bit_dummy)
-fdi$defense.max.x <- as.numeric(fdi$defense.max.x)
-
-#drop 2001 with missing variables
-fdi <- na.omit(fdi)
-
-#write csv
-#write.csv(fdi, file = "sub_stock.csv")
-
-
+########### Correlation Matrix #################
+source("correlation_function.R")
+var_list <- c(6:8, 10,12:16,19,25)
+corstars(fdi[,var_list], method=c("pearson"), removeTriangle=c("upper"),result=c("latex"))
+#p < .0001 ‘****’; p < .001 ‘***’, p < .01 ‘**’, p < .05 ‘*’
+########### Distribution Plots #################
 
 #plot dependent variable distribution
 fdi$Value_h <- ifelse(fdi$Value < 0, 0, fdi$Value)
 fdi_col <- subset(fdi, fdi$Value_ln == 0)
 fdi_h <- subset(fdi, fdi$Value_ln != 0)
 fdi_col$Value  <- 1
+n <- length(unique(fdi$Origin))
 fdi_col <- summaryBy(Value~Year, data = fdi_col, FUN=sum)
-fdi_col$prop <- fdi_col$Value.sum/15500
+fdi_col$prop <- fdi_col$Value.sum/(n*(n-1))
 #hist_1 <- hist(fdi$Value_h)
 #hist_1$counts <- hist_1$counts/12
 #plot(hist_1+1, col="lightgray", main = "Distribution of FDI Flows", ylim = c(0,600),
 #     xlab="Value, logged (Excludes Zero Values, ~85% of obs.)", ylab = "Per Year")
+pdf("descriptive_plots/fdi_stock_zeroes_boxplot.pdf",height=4.25,width=5.5,family="Times")
 par(mar = c(5,5,5,5))
 boxplot(fdi_h$Value_h~fdi_h$Year, log = "y",col="lightgray", main = "Non-Zero FDI Stocks",yaxt='n',
         xlab="Year", ylab = "FDI Stock (in mil USD)",outline=TRUE, cex=0.5, pch = 16, par(las=1))
@@ -75,66 +41,117 @@ plot(fdi_col$prop, axes=F, xlab=NA, ylab=NA, type="l", col = "#D55E00", pch = 16
 axis(side = 4, col = "#D55E00", col.axis ="#D55E00",col.ticks="#D55E00")
 par(las=0)
 mtext(side = 4, line = 3, 'Proportion of Zeroes', col ="#D55E00")
-
+dev.off()
 rm(fdi_col, fdi_h)
-
-
-
 
 
 ####### Descriptive Stats ##################################################################
 
 # Bar charts giving proportion 1 by year for dichotomous variables
 
-
-dichot <- summaryBy(bit_dummy + alliance + defense.max.x~Year, data = fdi, FUN=sum)
-dichot[,2:4] <- dichot[,2:4]/(length(fdi$Year)/length(unique(fdi$Year)))
+dichot <- summaryBy(bit_dummy + alliance + defense.max.x + both_oecd~Year, data = fdi, FUN=sum)
+dichot[,2:5] <- dichot[,2:5]/(length(fdi$Year)/length(unique(fdi$Year)))
 # BIT
-mp <- barplot(dichot$bit_dummy.sum,  main="Proportion of BIT dyads",
+pdf("descriptive_plots/BIT_barplot.pdf",height=4.25,width=5.5,family="Times")
+mp <-barplot(dichot$bit_dummy.sum,  main="Proportion of BIT dyads",
               col="lightgray",axes = FALSE,xlab="Year")
 axis(2,at=seq(0, .1, .02),labels=c("0%","2%","4%", "6%", "8%", "10%"))
 axis(1,at=mp,labels=dichot$Year)
+dev.off()
 # Alliance
-mp <- barplot(dichot$alliance.sum,  main="Proportion of Alliance Dyads",
+pdf("descriptive_plots/alliance_barplot.pdf",height=4.25,width=5.5,family="Times")
+mp <-barplot(dichot$alliance.sum,  main="Proportion of Alliance Dyads",
               col="lightgray",axes=FALSE,xlab="Year", ylim = c(0,.1))
 axis(2,at=seq(0, .1, .02),labels=c("0%","2%","4%", "6%", "8%", "10%"))
 axis(1,at=mp,labels=dichot$Year)
+dev.off()
 #Defense Treaty
-mp <- barplot(dichot$defense.max.x.sum,  main="Proportion of Defense Treaty Dyads",
+pdf("descriptive_plots/defense_barplot.pdf",height=4.25,width=5.5,family="Times")
+mp <-barplot(dichot$defense.max.x.sum,  main="Proportion of Defense Treaty Dyads",
               col="lightgray",axes = FALSE,xlab="Year", ylim = c(0,.1))
 axis(2,at=seq(0, .1, .02),labels=c("0%","2%","4%", "6%", "8%", "10%"))
 axis(1,at=mp,labels=dichot$Year)
+dev.off()
+#OECD pairs
+pdf("descriptive_plots/OECDpair_barplot.pdf",height=4.25,width=5.5,family="Times")
+mp <-barplot(dichot$both_oecd.sum,  main="Proportion of Defense Treaty Dyads",
+        col="lightgray",axes = FALSE,xlab="Year", ylim = c(0,.1))
+axis(2,at=seq(0, .1, .02),labels=c("0%","2%","4%", "6%", "8%", "10%"))
+axis(1,at=mp,labels=dichot$Year)
+dev.off()
 
-rm(dichot, mp)
+rm(dichot,mp)
 # Yearly box-plots for quantitative variables
 
 #Polity
+pdf("descriptive_plots/polity_boxplot.pdf",height=4.25,width=5.5,family="Times")
 boxplot(fdi$Dest.polity~fdi$Year, col="lightgray", main = "Polity",#yaxt='n',
         xlab="Year", ylab = "Polity Scale", cex=0.5, pch = 16)
+dev.off()
 #Trade_openness
+pdf("descriptive_plots/TO_boxplot.pdf",height=4.25,width=5.5,family="Times")
 boxplot(fdi$Dest.TO~fdi$Year, col="lightgray", main = "Trade Openness", yaxt='n',
         xlab="Year", ylab = "IM/EX as % of GDP ", cex=0.5, pch = 16)
 axis(2,at=seq(0, 400, 100),labels=c("0%","100%","200%", "300%", "400%"))
-#Population
-boxplot(fdi$Dest.pop~fdi$Year, log = "y",col="lightgray", main = "Population", yaxt='n',
-        xlab="Year", ylab = "Population in Millions", cex=0.5, pch = 16)
-axis(2,at=c(0, 1, 100, 1000),labels=c("0","1","100","1K"))
+dev.off()
 #gdp.pc
+pdf("descriptive_plots/GDPpc_boxplot.pdf",height=4.25,width=5.5,family="Times")
 boxplot(fdi$Dest.GDPpc~fdi$Year, col="lightgray", main = "GDP pc", yaxt='n',
-        xlab="Year", ylab = "GDP per capita", cex=0.5, pch = 16)
-axis(2,at=c(0, 50000, 100000, 150000),labels=c("0","50K","100K","150K"))
+        xlab="Year", ylab = "GDP per capita in USD, logged", cex=0.5, pch = 16)
+axis(2,at=seq(0,12,2),labels=c("0","2","4","6","8","10","12"))
+dev.off()
 #Mass
+pdf("descriptive_plots/Mass_boxplot.pdf",height=4.25,width=5.5,family="Times")
 boxplot(fdi$mass~fdi$Year, col="lightgray", main = "Mass", #yaxt='n',
-        xlab="Year", ylab = "GDP Product of Dyad, Logged", cex=0.5, pch = 16)
+        xlab="Year", ylab = "GDP Product of Dyad, logged", cex=0.5, pch = 16)
 axis(2,at=c(0, 50000, 100000, 150000),labels=c("0","50K","100K","150K"))
+dev.off()
 #Distance
+pdf("descriptive_plots/dist_boxplot.pdf",height=4.25,width=5.5,family="Times")
 boxplot(fdi$dist~fdi$Year, col="lightgray", main = "Distance", yaxt='n',
-        xlab="Year", ylab = "Distance, Miles", cex=0.5, pch = 16)
-axis(2,at=c(0, 5000, 10000, 15000, 20000),labels=c("0","5K","10K","15K", "20K"))
+        xlab="Year", ylab = "Distance in Miles, logged", cex=0.5, pch = 16)
+axis(2,at=c(0, 2.5, 5, 7.5, 10),labels=c("0","2.5","5","7.5", "10"))
+dev.off()
 #Trade
-boxplot(fdi$trade_ln~fdi$Year, col="lightgray", main = "Trade", #yaxt='n',
+pdf("descriptive_plots/Trade_boxplot.pdf",height=4.25,width=5.5,family="Times")
+boxplot(fdi$trade_ln~fdi$Year, col="lightgray", main = "Bilateral Trade Volumne", #yaxt='n',
         xlab="Year", ylab = "Value of Trade USD in mil, logged", cex=0.5, pch = 16)
 axis(2,at=c(0, 50000, 100000, 150000),labels=c("0","50K","100K","150K"))
+dev.off()
+#PTA Depth
+pdf("descriptive_plots/ptadepth_boxplot.pdf",height=4.25,width=5.5,family="Times")
+boxplot(fdi$trade_ln~fdi$Year, col="lightgray", main = "PTA Depth", #yaxt='n',
+        xlab="Year", ylab = "Latent Depth of PTA", cex=0.5, pch = 16)
+axis(2,at=c(0, 50000, 100000, 150000),labels=c("0","50K","100K","150K"))
+dev.off()
+
+#check summed world totals
+
+fdi2 <- read.csv("../../create_datasets/clean_stock.csv", stringsAsFactors=FALSE) #FDI
+df1 <- fdi[,c(1:3,5)]
+df1$Value <- ifelse(is.na(df1$Value),0, df1$Value)
+df1 <- summaryBy(Value ~ Destination +Year,data=df1, FUN = sum, keep.names = T)
+df1$Source <- "Sum"
+df1 <- df1[,c(1,4,2,3)]
+df2 <- subset(fdi2, fdi2$Origin=="World")[,c(2,4,6,7)]
+df2$Value <- ifelse(is.na(df2$Value),0, df2$Value)
+colnames(df2)[2] <- "Source"
+
+df1 <- subset(df1, df1$Destination %in% df2$Destination)
+df2 <- subset(df2, df2$Destination %in% df1$Destination)
+
+df <- rbind(df1, df2)
 
 
-#clean_stock.csv has world totals
+name <- ggplot(df ,aes(x=(Value), fill=Source)) + 
+  geom_density(alpha=0.25)+ 
+  scale_fill_manual(values=c("red","blue"),name = "Group")+
+  ggtitle(paste0("Distribution of World vs.Partner Sums"))+
+  #geom_vline(xintercept=max(d$donation.sum)/1000, colour="red") +
+  #geom_vline(xintercept=max(r$donation.sum)/1000, colour="blue") +
+  #geom_text(aes(x=max(d$donation.sum)/1000, label="Max Contribution Total, BE", y=0.05), 
+  #          colour="black", angle=90, vjust = 1.2, family="URWTimes")+
+  #geom_text(aes(x=max(r$donation.sum)/1000, label="Max Contribution Total, Random", y=0.05), 
+  #          colour="black", angle=90, vjust = 1.2, family="URWTimes")+
+  ylab("Density")+ xlab("FDI Value, USD")+
+ggsave(paste("descriptive_plots/check_sums.pdf",sep=""), name, device="pdf", width=6, height=4)
